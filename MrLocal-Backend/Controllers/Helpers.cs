3d9c3602be62b5cf6fda814bcee0696c7857a1d3
Skarulis Justinas <justinas.skarulis@mif.stud.vu.lt>
2020-11-15 23:34:44 +0200

    Refactoring

--- MrLocal-Backend/Controllers/Helpers.cs
+++ MrLocal-Backend/Controllers/Helpers.cs
@@ -12,12 +12,12 @@
 
         public class GetShopArguments
         {
-            public string Id { get;  set; }
-            public string Name { get;  set; }
-            public string Status { get;  set; }
-            public string Description { get;  set; }
-            public string TypeOfShop { get;  set; }
-            public string City { get;  set; }
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string Status { get; set; }
+            public string Description { get; set; }
+            public string TypeOfShop { get; set; }
+            public string City { get; set; }
         }
     }
 }
