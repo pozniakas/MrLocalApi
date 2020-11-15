6aa6b3bfa786d2f94b274b7d4099c79e6412f8b5
Skarulis Justinas <justinas.skarulis@mif.stud.vu.lt>
2020-11-15 23:56:20 +0200

    Fixed validaton

--- MrLocal-Backend/Services/ShopService.cs
+++ MrLocal-Backend/Services/ShopService.cs
@@ -70,11 +70,11 @@ namespace MrLocal_Backend.Services
             var nameRegex = new Regex(@"^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$");
             var shops = shopRepository.FindAll();
 
-            var isValidName = (isUpdate && name == "") || (name.Length > 2 && nameRegex.IsMatch(name) && shops.Where(i => i.Name == name).Count() == 0);
-            var isValidStatus = (isUpdate && status == "") || Array.Exists(arrayOfStatusTypes, i => i == status) || (!isUpdate && status == null);
-            var isValidDescription = (isUpdate && description == "") || (description.Length > 2);
-            var isValidTypeOfShop = (isUpdate && typeOfShop == "") || Array.Exists(arrayOfShopTypes, i => i == typeOfShop);
-            var isValidCity = (isUpdate && city == "") || Array.Exists(arrayOfCities, i => i == city);
+            var isValidName = (isUpdate && name != "") || (name.Length > 2 && nameRegex.IsMatch(name) && shops.Where(i => i.Name == name).Count() == 0);
+            var isValidStatus = (isUpdate && status != "") || Array.Exists(arrayOfStatusTypes, i => i == status) || (!isUpdate && status == null);
+            var isValidDescription = (isUpdate && description != "") || (description.Length > 2);
+            var isValidTypeOfShop = (isUpdate && typeOfShop != "") || Array.Exists(arrayOfShopTypes, i => i == typeOfShop);
+            var isValidCity = (isUpdate && city != "") || Array.Exists(arrayOfCities, i => i == city);
 
             return isValidName && isValidStatus && isValidTypeOfShop && isValidCity && isValidDescription;
         }
