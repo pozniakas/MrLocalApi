ea9f0220e389ddf872d76601bd1174a3005c2bc6
Skarulis Justinas <justinas.skarulis@mif.stud.vu.lt>
2020-11-16 16:04:16 +0200

    Added ValidateData to Helpers folder in services and Refactored Product service

---
+++ MrLocal-Backend/Services/Helpers/ValidateData.cs
@@ -0,0 +1,34 @@
+﻿using MrLocal_Backend.Repositories;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace MrLocal_Backend.Services.Helpers
+{
+    public class ValidateData
+    {
+        private readonly ShopRepository shopRepository;
+        private readonly ProductRepository productRepository;
+
+        public ValidateData()
+        {
+            shopRepository = new ShopRepository();
+            productRepository = new ProductRepository();
+        }
+        public bool ValidateProductData(string shopId, string name, string description, double? price, bool isUpdate, string priceType, string id = null)
+        {
+            var nameRegex = new Regex(@"^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$");
+            var priceRegex = new Regex(@"\d+(?:\.\d+)?");
+            var shops = shopRepository.FindOne(shopId);
+            var priceTypes = new List<string> { "UNIT", "GRAMS", "KILOGRAMS" };
+
+            var doesProductExist = (id != null) && (productRepository.FindOne(id) != null) || id == null;
+            var isValidShop = shops != null;
+            var isValidName = (isUpdate && name == null) || (name.Length > 2 && nameRegex.IsMatch(name));
+            var isValidDescription = (isUpdate && description == null) || (description.Length > 2);
+            var isValidPrice = priceRegex.IsMatch(price.ToString()) || (isUpdate && price == null);
+            var isValidPriceType = priceTypes.Contains(priceType) || (isUpdate && priceType == null);
+
+            return isValidName && isValidDescription && isValidPrice && isValidShop && doesProductExist && isValidPriceType;
+        }
+    }
+}
--- MrLocal-Backend/Services/ProductService.cs
+++ MrLocal-Backend/Services/ProductService.cs
@@ -1,24 +1,23 @@
 ﻿using MrLocal_Backend.Repositories;
+using MrLocal_Backend.Services.Helpers;
 using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
 
 namespace MrLocal_Backend.Services
 {
     class ProductService
     {
-        private readonly ShopRepository shopRepository;
         private readonly ProductRepository productRepository;
+        private readonly ValidateData validateData;
 
         public ProductService()
         {
-            shopRepository = new ShopRepository();
             productRepository = new ProductRepository();
+            validateData = new ValidateData();
         }
 
         public void AddProductToShop(string shopId, string name, string description, string priceType, double? price)
         {
-            if (ValidateProductData(shopId, name, description, price, false, priceType))
+            if (validateData.ValidateProductData(shopId, name, description, price, false, priceType))
             {
                 productRepository.Create(shopId, name, description, priceType, price);
             }
@@ -30,7 +29,7 @@ namespace MrLocal_Backend.Services
 
         public void UpdateProduct(string id, string shopId, string name, string description, string priceType, double? price)
         {
-            if (ValidateProductData(shopId, name, description, price, true, priceType, id))
+            if (validateData.ValidateProductData(shopId, name, description, price, true, priceType, id))
             {
                 productRepository.Update(id, shopId, name, description, priceType, price);
             }
@@ -52,23 +51,6 @@ namespace MrLocal_Backend.Services
             {
                 throw new ArgumentException("Invalid products parameters for creation");
             }
-        }
-
-        private bool ValidateProductData(string shopId, string name, string description, double? price, bool isUpdate, string priceType, string id = null)
-        {
-            var nameRegex = new Regex(@"^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$");
-            var priceRegex = new Regex(@"\d+(?:\.\d+)?");
-            var shops = shopRepository.FindOne(shopId);
-            var priceTypes = new List<string> { "UNIT", "GRAMS", "KILOGRAMS" };
-
-            var doesProductExist = (id != null) && (productRepository.FindOne(id) != null) || id == null;
-            var isValidShop = shops != null;
-            var isValidName = (isUpdate && name == null) || (name.Length > 2 && nameRegex.IsMatch(name));
-            var isValidDescription = (isUpdate && description == null) || (description.Length > 2);
-            var isValidPrice = priceRegex.IsMatch(price.ToString()) || (isUpdate && price == null);
-            var isValidPriceType = priceTypes.Contains(priceType) || (isUpdate && priceType == null);
-
-            return isValidName && isValidDescription && isValidPrice && isValidShop && doesProductExist && isValidPriceType;
         }
     }
 }
