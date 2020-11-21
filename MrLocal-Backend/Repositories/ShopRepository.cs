967b55da7a812129d646bee5c6f3f76f506fd507
Skarulis Justinas <justinas.skarulis@mif.stud.vu.lt>
2020-11-21 22:33:30 +0200

    Refactoring

--- MrLocal-Backend/Repositories/ShopRepository.cs
+++ MrLocal-Backend/Repositories/ShopRepository.cs
@@ -5,19 +5,20 @@ using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 using System.Configuration;
+using MrLocal_Backend.Repositories.Interfaces;
 
 namespace MrLocal_Backend.Repositories
 {
-    public class ShopRepository : XmlRepository<ShopRepository>
+    public class ShopRepository : XmlRepository<ShopRepository>, IShopRepository
     {
         readonly string fileName;
 
-        public string Id { get; private set; }
-        public string Name { get; private set; }
-        public string Status { get; private set; }
-        public string Description { get; private set; }
-        public string TypeOfShop { get; private set; }
-        public string City { get; private set; }
+        public string Id { get;  set; }
+        public string Name { get;  set; }
+        public string Status { get;  set; }
+        public string Description { get;  set; }
+        public string TypeOfShop { get;  set; }
+        public string City { get;  set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
