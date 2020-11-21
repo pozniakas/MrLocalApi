bcc7e5dc4e70facc50b188cf47a4d9a3be397e6b
Skarulis Justinas <justinas.skarulis@mif.stud.vu.lt>
2020-11-21 23:03:08 +0200

    Added properties to IShopRep.. and IProductRep..

--- MrLocal-Backend/Repositories/Interfaces/IproductRepository.cs
+++ MrLocal-Backend/Repositories/Interfaces/IproductRepository.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static MrLocal_Backend.Repositories.ProductRepository;
 
 namespace MrLocal_Backend.Repositories.Interfaces
 {
     interface IProductRepository : IRepository
     {
+        public string ShopId { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public PriceTypes PriceType { get; set; }
+
         public void Create(string shopId, string name, string description, string pricetype, double? price);
         public void Update(string id, string shopId, string name, string description, string pricetype, double? price);
         public ProductRepository FindOne(string id);
