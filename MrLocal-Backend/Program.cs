c45d6e0f9b2f4ddcca96de10cb80c675a83142ab
Skarulis Justinas <justinas.skarulis@mif.stud.vu.lt>
2020-11-21 16:34:17 +0200

    Added C# 9.0 features

--- MrLocal-Backend/Program.cs
+++ MrLocal-Backend/Program.cs
@@ -1,20 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using MrLocal_Backend;
 
-namespace MrLocal_Backend
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+CreateHostBuilder(args).Build().Run();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+static IHostBuilder CreateHostBuilder(string[] args) =>
+   Host.CreateDefaultBuilder(args)
+       .ConfigureWebHostDefaults(webBuilder =>
+       {
+           webBuilder.UseStartup<Startup>();
+       });
+
