9075a92cc11707b09a3509ae24b7e2f36e793306
Skarulis Justinas <justinas.skarulis@mif.stud.vu.lt>
2020-11-21 23:04:11 +0200

    Refactoring with ctrl + k+ e

--- MrLocal-Backend/Repositories/Interfaces/IRepository.cs
+++ MrLocal-Backend/Repositories/Interfaces/IRepository.cs
@@ -4,12 +4,12 @@ namespace MrLocal_Backend.Repositories.Interfaces
 {
     interface IRepository
     {
-        public string Id { get;  set; }
-        public string Name { get;  set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
-        
-        public void Delete(string id);     
+
+        public void Delete(string id);
     }
 }
