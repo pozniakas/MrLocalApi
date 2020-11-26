848c2b2a3c346b34ecb2237907d7a73444d321aa
Skarulis Justinas <justinas.skarulis@mif.stud.vu.lt>
2020-11-27 01:09:55 +0200

    deleted unnecessary funcion

--- MrLocal-Backend/Extencions/ExceptionMiddlewareExtensions.cs
+++ MrLocal-Backend/Extencions/ExceptionMiddlewareExtensions.cs
@@ -13,28 +13,6 @@ namespace MrLocal_Backend.Extencions
         {
             app.UseMiddleware<ExceptionMiddleware>();
         }
-
-        public static void ConfigureExceptionHandler(this IApplicationBuilder app, ILoggerManager logger)
-        {
-            app.UseExceptionHandler(appError =>
-            {
-                appError.Run(async context =>
-                {
-                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                    context.Response.ContentType = "application/json";
-                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
-                    if (contextFeature != null)
-                    {
-                        logger.LogError($"Something went wrong: {contextFeature.Error}");
-                        await context.Response.WriteAsync(new ErrorDetails()
-                        {
-                            StatusCode = context.Response.StatusCode,
-                            Message = "Internal Server Error."
-                        }.ToString());
-                    }
-                });
-            });
-        }
     }
 }
 
