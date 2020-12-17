6fd37e7435d46768419a255e1539369d089c01c6
Domas Pozniakas <domaspoz@gmail.com>
2020-12-17 09:02:27 +0200

    Removed not used folders

--- MrLocal.Backend/Repositories/Interfaces/IEnumConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿using static MrLocalBackend.Models.Product;
-
-namespace MrLocalBackend.Repositories.Interfaces
-{
-    public interface IEnumConverter
-    {
-        public PriceTypes StringToPricetype(string pricetype);
-    }
-}
