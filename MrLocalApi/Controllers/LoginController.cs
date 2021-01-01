4585159f332c459f91b7f15298dfefef99663188
Bražionis Ignas <ignas.brazionis@mif.stud.vu.lt>
2021-01-02 00:37:27 +0200

    Added login and register controllers

---
+++ MrLocalApi/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MrLocalApi.Controllers.Interfaces;
+using System.Threading.Tasks;
+using static MrLocalBackend.Models.Body;
+
+namespace MrLocalApi.Controllers
+{
+    [Route("api/login")]
+    [ApiController]
+    public class LoginController : ControllerBase, ILogin
+    {
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] UserCred userCred)
+        {
+            return Ok();
+        }
+    }
+}
