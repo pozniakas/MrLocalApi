a6de01a9ac7df991501812b6c7841e31c9ea09dd
Bražionis Ignas <ignas.brazionis@mif.stud.vu.lt>
2021-01-04 20:01:11 +0200

    Added unit test

---
+++ MrLocalUnitTests/MrLocalValidateDataUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using Xunit;
+using MrLocalBackend.Services.Interfaces;
+
+
+namespace MrLocalUnitTests
+{
+    public class MrLocalValidateDataUnitTests
+    {
+        [Theory]
+        [InlineData("4564654", "shop", "description", "5.1" , false, "Berries")]
+
+        public async System.Threading.Tasks.Task ValidateData_ValidateProductData_ReturnsTrueIfEnteredFieldsAreValidAsync(string shopId, string name, string description, string priceString, bool isUpdate, string priceType)
+        {
+            decimal? price = priceString == null ? null : decimal.Parse(priceString);
+
+            var validateDataMock = new Mock<IValidateData>();
+
+            var result = await validateDataMock.Object.ValidateProductData(shopId, name, description, price, isUpdate, priceType);
+
+            Assert.True(result);
+        }
+    }
+}
