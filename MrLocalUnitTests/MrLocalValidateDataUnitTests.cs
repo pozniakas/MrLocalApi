9eff5c668ae30d32e0dea87b8ccd00203361b0c0
Skarulis Justinas <justinas.skarulis@mif.stud.vu.lt>
2021-01-04 20:30:54 +0200

    Added test for enumconverter

--- MrLocalUnitTests/MrLocalValidateDataUnitTests.cs
+++ MrLocalUnitTests/MrLocalValidateDataUnitTests.cs
@@ -1,24 +1,49 @@
 ﻿using Moq;
+using MrLocalBackend.Repositories.Helpers;
+using MrLocalBackend.Repositories.Interfaces;
+using MrLocalBackend.Services.Helpers;
+using MrLocalDb.Entities;
+using System.Threading.Tasks;
 using Xunit;
-using MrLocalBackend.Services.Interfaces;
-
 
 namespace MrLocalUnitTests
 {
     public class MrLocalValidateDataUnitTests
     {
         [Theory]
-        [InlineData("4564654", "shop", "description", "5.1" , false, "Berries")]
-
-        public async System.Threading.Tasks.Task ValidateData_ValidateProductData_ReturnsTrueIfEnteredFieldsAreValidAsync(string shopId, string name, string description, string priceString, bool isUpdate, string priceType)
+        [InlineData("4564654", "shop", "description", "5.1", false, "Berries")]
+        public async Task ValidateData_ValidateProductData_ReturnsTrueIfEnteredFieldsAreValidAsync(string shopId, string name, string description, string priceString, bool isUpdate, string priceType)
         {
             decimal? price = priceString == null ? null : decimal.Parse(priceString);
 
-            var validateDataMock = new Mock<IValidateData>();
+            //Arange
+            var validateShopRepMock = new Mock<IShopRepository>();
+            var validateUserMock = new Mock<IUserRepository>();
+            var validateData = new ValidateData(validateShopRepMock.Object, validateUserMock.Object);
 
-            var result = await validateDataMock.Object.ValidateProductData(shopId, name, description, price, isUpdate, priceType);
+            //Act
+            var result = await validateData.ValidateProductData(shopId, name, description, price, isUpdate, priceType);
 
+            //Assert
             Assert.True(result);
         }
+
+        [Theory]
+        [InlineData(PriceTypes.KILOGRAMS, "KILOGRAMS")]
+        [InlineData(PriceTypes.UNIT, "UNIT")]
+        [InlineData(PriceTypes.GRAMS, "GRAMS")]
+        [InlineData(PriceTypes.UNIT, "KILOGRAMS")]
+        public void EnumConverter_StringToPricetype_ReturnsCorrectExpresionOfPriceTypes(PriceTypes realPriceType, string priceType)
+        {
+            //Arange
+            var enumConverter = new EnumConverter();
+
+            //Act
+            var result = enumConverter.StringToPricetype(priceType);
+
+            //Assert
+            Assert.Equal(realPriceType, result);
+        }
+
     }
 }
